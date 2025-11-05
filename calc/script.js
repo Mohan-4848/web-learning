97ab703de58362307c8239771437d5a32a783618
Mohan-4848 <mohanbalaji4848@gmail.com>
2025-11-05 21:11:55 +0530

    added removeLast function

--- calc/script.js
+++ calc/script.js
@@ -4,6 +4,10 @@ function appendToDisplay(input){
     display.value += input
 }
 
+function removeLastDigit(){
+    display.value = display.value.slice(0, -1);
+}
+
 function execute(){
     try{
         display.value = eval(display.value)
